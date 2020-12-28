c00e8a3518dd9442d08eddc1b408fe79f38e9f69
khampanpare88 <khampanpare88@gmail.com>
2020-12-28 13:55:54 +0700

    1

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,4 +1,9 @@
 export const CourseCard = (props) => {
   // TODO: design HTML component that displays a course as a "card" on the webpage.
-  return (null);
+  return <div className = "bg-blue-100 rounded-2xl p-1  hover:bg-red-200">
+  <p>{props.name}</p>
+  <p>{props.grd}</p>
+  <p>{props.crd}</p>
+  <button onClick = {() =>props.del(props.name)} > -delete- </button>
+  </div>;
 };
